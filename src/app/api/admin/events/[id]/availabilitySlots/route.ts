972bc329039f3deb4b079e394a5866d380e12181
Giorgi Garsevanishvili <George.Garsevanidi@Gmail.com>
2026-03-27 17:22:07 +0200

    feat: Event Related AvailabilitySlots GET API Endpoint Added -- Added All Event Related AvailabilitySlots -- build Errors Fixed

--- src/app/api/admin/events/[id]/availabilitySlots/route.ts
+++ src/app/api/admin/events/[id]/availabilitySlots/route.ts
@@ -1,12 +1,60 @@
+import { AvailabilitySlotWhereInput } from "@/generated/models";
 import { handleError } from "@/lib/errors/handleErrors";
 import prisma from "@/lib/prisma";
 import { requireRole } from "@/lib/serverAuth";
 import { CreateAvailabilitySlot } from "@/lib/zod";
 import { Context } from "@/types/general-types";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import z from "zod";
 
-export const POST = async (req: NextResponse, context: Context) => {
+export const GET = async (_req: NextRequest, context: Context) => {
+  try {
+    const thisUser = await requireRole("ADMIN");
+    const { id } = await context.params;
+
+    if (!id) {
+      return NextResponse.json({ message: "Id is missing" });
+    }
+
+    const whereClause: AvailabilitySlotWhereInput = {
+      eventId: id,
+      event: { regionId: thisUser.user.ownAllowance?.regionId },
+    };
+
+    const data = await prisma.availabilitySlot.findMany({
+      where: whereClause,
+      include: {
+        CreatedBy: { select: { name: true } },
+        updatedBy: { select: { name: true } },
+        role: { select: { name: true } },
+        event: {
+          select: {
+            name: true,
+            region: { select: { name: true } },
+            finalizedAt: true,
+          },
+        },
+        availabilityEntries: {
+          select: { user: { select: { name: true } }, status: true },
+        },
+      },
+    });
+
+    if (!data) {
+      return NextResponse.json({
+        data,
+        message: `Availability Slot For Event With ID: ${id} not found!`,
+      });
+    }
+
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    const { message, status } = handleError(error);
+    return NextResponse.json({ message }, { status });
+  }
+};
+
+export const POST = async (req: NextRequest, context: Context) => {
   try {
     const thisUser = await requireRole("ADMIN");
     const { id } = await context.params;
